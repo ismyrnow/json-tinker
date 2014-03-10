0de49f03d33bdd22e28c3ab2fb9f5504d02926d5
Ishmael Smyrnow <ismyrnow@gmail.com>
2014-03-09 22:01:28 -0400

    Add yaml to tabbed demo

--- demo/tabbed.js
+++ demo/tabbed.js
@@ -1,33 +1,39 @@
 $(function () {
   
   var $raw = $('#config-raw');
+  var $yaml = $('#config-yaml');
   var $visual = $('#config-visual').tinker({ json: $raw.val() });
   
-  $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {    
-    if (e.target.hash === '#raw') {
-      $raw.val($visual.tinker('json'));
-    } else {
-      $visual.tinker({ json: $raw.val() });
+  $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+    var activatedTab = e.target.hash;
+    var previousTab = e.relatedTarget.hash;
+    var json;
+    
+    // Get json from previous tab.
+    switch (previousTab) {
+      case '#visual':
+        json = $visual.tinker('json');
+        break;
+      case '#yaml':
+        json = JSON.stringify(jsyaml.load($yaml.val()), null, 2);
+        break;
+      default:
+        json = $raw.val();
+        break;
+    }
+    
+    // Update activated tab with latest json.
+    switch (activatedTab) {
+      case '#visual':
+        $visual.tinker({ json: json });
+        break;
+      case '#yaml':
+        $yaml.val(jsyaml.dump(JSON.parse(json)));
+        break;
+      case '#raw':
+        $raw.val(json);
+        break;
     }
   });
-  
-//  $(document).bind('contextmenu', function () {
-//    return false;
-//  }); 
-//
-//  $('#load-json').on('click', function () {
-//    var json = $input.val();
-//    $editor.tinker({ json: json });
-//  });
-//
-//  $('#extract-json').on('click', function () {
-//    var json = $editor.tinker('json');
-//    $input.val(json);
-//  });
-//
-//  $input.click(function () {
-//    $(this).focus();
-//    $(this).select();
-//  });
   
 });
